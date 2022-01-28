d43add51b57cfc958871f84062a15bd56d1deea8
Pragathi-S-S <97836304+Pragathi-S-S@users.noreply.github.com>
2022-01-28 11:55:17 +0530

    Add files via upload

---
+++ ProaccessSpecifiers.java
@@ -0,0 +1,15 @@
+package pack1;
+
+public class ProaccessSpecifiers {
+
+	//3. using protected access specifiers
+	
+
+		protected void display() 
+	    { 
+	        System.out.println("This is protected access specifier"); 
+	    } 
+	}
+
+
+
---
+++ PublicaccessSpecifiers.java
@@ -0,0 +1,16 @@
+package pack1;
+
+public class PublicaccessSpecifiers {
+
+		
+			//4. using public access specifiers
+			
+				public void display() 
+			    { 
+			        System.out.println("This is Public Access Specifiers"); 
+			    } 
+			}
+
+		
+
+
