cdde13b50141eb4b01c6af56c798673bbe025b3c
Pragathi-S-S <97836304+Pragathi-S-S@users.noreply.github.com>
2022-01-28 12:16:06 +0530

    Add files via upload

---
+++ pack2/accessSpecifiers3.java
@@ -0,0 +1,19 @@
+package pack2;
+
+
+	import pack1.*;
+
+	import pack1.ProaccessSpecifiers;
+
+			public class accessSpecifiers3 extends ProaccessSpecifiers {
+
+				public static void main(String[] args) {
+					accessSpecifiers3 obj = new accessSpecifiers3 ();   
+				       obj.display();  
+				}
+
+			}
+
+		
+
+
---
+++ pack2/accessSpecifiers4.java
@@ -0,0 +1,18 @@
+package pack2;
+
+
+	import pack1.*;
+
+	import pack1.PublicaccessSpecifiers;
+	public class accessSpecifiers4 {
+
+			//create another package
+		
+
+				public static void main(String[] args) {
+					
+					PublicaccessSpecifiers obj = new PublicaccessSpecifiers(); 
+			        obj.display();  
+					
+				}
+			}
